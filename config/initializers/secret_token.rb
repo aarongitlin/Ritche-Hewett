55d21db144bbe00e757dcaff9a4de1f301729e5c
Jonas Rademar <jonas.rademar@gmail.com>
2012-08-24 17:46:16 +0200

    new file:   3.0.0' 	new file:   Gemfile 	new file:   Gemfile.lock 	new file:   README.rdoc 	new file:   Rakefile 	new file:   app/assets/images/rails.png 	new file:   app/assets/javascripts/application.js 	new file:   app/assets/stylesheets/application.css 	new file:   app/controllers/application_controller.rb 	new file:   app/decorators/controllers/refinery/.gitkeep 	new file:   app/decorators/models/refinery/.gitkeep 	new file:   app/helpers/application_helper.rb 	new file:   app/mailers/.gitkeep 	new file:   app/models/.gitkeep 	new file:   app/views/layouts/application.html.erb 	new file:   app/views/refinery/_footer.html.erb 	new file:   app/views/refinery/_header.html.erb 	new file:   app/views/refinery/_menu.html.erb 	new file:   app/views/refinery/_menu_branch.html.erb 	new file:   app/views/refinery/pages/show.html.erb 	new file:   app/views/sitemap/index.xml.builder 	new file:   config.ru 	new file:   config/application.rb 	new file:   config/boot.rb 	new file:   config/database.yml 	new file:   config/database.yml.mysql 	new file:   config/database.yml.postgresql 	new file:   config/database.yml.sqlite3 	new file:   config/environment.rb 	new file:   config/environments/development.rb 	new file:   config/environments/production.rb 	new file:   config/environments/test.rb 	new file:   config/initializers/backtrace_silencers.rb 	new file:   config/initializers/inflections.rb 	new file:   config/initializers/mime_types.rb 	new file:   config/initializers/refinery/authentication.rb 	new file:   config/initializers/refinery/core.rb 	new file:   config/initializers/refinery/i18n.rb 	new file:   config/initializers/refinery/images.rb 	new file:   config/initializers/refinery/pages.rb 	new file:   config/initializers/refinery/resources.rb 	new file:   config/initializers/secret_token.rb 	new file:   config/initializers/session_store.rb 	new file:   config/initializers/wrap_parameters.rb 	new file:   config/locales/en.yml 	new file:   config/routes.rb 	new file:   db/migrate/20120819071642_create_refinerycms_authentication_schema.refinery_authentication.rb 	new file:   db/migrate/20120819071643_create_refinerycms_images_schema.refinery_images.rb 	new file:   db/migrate/20120819071644_create_refinerycms_resources_schema.refinery_resources.rb 	new file:   db/migrate/20120819071645_create_refinerycms_pages_schema.refinery_pages.rb 	new file:   db/migrate/20120819071646_create_seo_meta.seo_meta_engine.rb 	new file:   db/migrate/20120819072638_create_testimonials_testimonials.refinery_testimonials.rb 	new file:   db/schema.rb 	new file:   db/seeds.rb 	new file:   doc/README_FOR_APP 	new file:   lib/assets/.gitkeep 	new file:   lib/tasks/.gitkeep 	new file:   log/.gitkeep 	new file:   public/404.html 	new file:   public/422.html 	new file:   public/500.html 	new file:   public/favicon.ico 	new file:   public/robots.txt 	new file:   readme.txt 	new file:   script/rails 	new file:   vendor/assets/javascripts/.gitkeep 	new file:   vendor/assets/stylesheets/.gitkeep 	new file:   vendor/extensions/testimonials/Gemfile 	new file:   vendor/extensions/testimonials/Guardfile 	new file:   vendor/extensions/testimonials/Rakefile 	new file:   vendor/extensions/testimonials/app/controllers/refinery/testimonials/admin/testimonials_controller.rb 	new file:   vendor/extensions/testimonials/app/controllers/refinery/testimonials/testimonials_controller.rb 	new file:   vendor/extensions/testimonials/app/models/refinery/testimonials/testimonial.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RitchieHewett::Application.config.secret_token = '057091988426d4a42d132385a6de0e8646bb6fb78c2174c8856ce86d7b07ed0cc8a71d0ca68f91face1bc949a0ee6ee1b126e7e38bb51148b091c87239923caa'
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Guidelines for what to edit in the app\assets\stylesheets\application.css
+
+Testimonial:
+	Title (.testi_title)
+	Image (.testi_img)
+	Text_container (.testi_b_container)
+		Description text (.testi_b1)
+		Location & Time (.testi_b2)
